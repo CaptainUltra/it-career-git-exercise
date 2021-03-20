cac52b770f891b1e060decebc6751e794d60d984
Bozhkov <bozhkov.2002@gmail.com>
2021-03-20 12:09:57 +0200

    Dev A-completed

--- Startup.cs
+++ Startup.cs
@@ -4,8 +4,23 @@ namespace GitExercise
 {
     public class Startup
     {
+        private const string Password = "abcd1234";
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain accesss: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+            return password == Password;
+        }
         public static void Main()
         {
+            bool isAuthorized = CheckCredentials();
+            if (!isAuthorized)
+            {
+                Console.WriteLine("access denied.");
+                Console.ReadKey(true);
+                return;
+            }
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -35,8 +50,13 @@ namespace GitExercise
                 case "m":
                     OptionsManager.Multiply(a, b);
                     break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubstractAbs(a, b);
+                    break;
             }
-
             Console.WriteLine("Pres any key to close the app...");
             Console.ReadKey(true);
         }
